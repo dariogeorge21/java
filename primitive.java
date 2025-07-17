b026c267d16200a834b265454558391e33e8ef58
Dario George <edu.dariogeorge21@gmail.com>
2025-07-17 22:34:19 +0530

    primitive

---
+++ primitive.java
@@ -0,0 +1,20 @@
+public class primitive {
+    public static void main(String[] args){
+        byte b = 10;
+        short s = 20;
+        int i = 30;
+        long l = 40L;
+        float f = 50.0F;
+        double d = 60.0;
+        char c = 'a';
+        boolean bool = true;
+        System.out.println("Byte: "+b);
+        System.out.println("Short: "+s);
+        System.out.println("Int: "+i);
+        System.out.println("Long: "+l);
+        System.out.println("Float: "+f);
+        System.out.println("Double: "+d);
+        System.out.println("Char: "+c);
+        System.out.println("Boolean: "+bool);
+    }   
+}

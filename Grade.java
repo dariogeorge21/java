1b7600aaf3af6a600b5312e8c084f5951292807b
Dario George <edu.dariogeorge21@gmail.com>
2025-07-17 22:06:06 +0530

    Grade scheme

--- Grade.java
+++ Grade.java
@@ -1,33 +1,32 @@
-package grade;
 import java.util.Scanner;
 
 public class Grade{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        System.print.out("Enter marks: ");
+        System.out.print("Enter marks: ");
         int mark = sc.nextInt();
 
         if (mark>0 && mark<100){
             if (mark>90){
-                System.print.out("S Grade\n");
+                System.out.print("S Grade\n");
             } else if (mark>80){
-                System.print.out("A Grade\n");
+                System.out.print("A Grade\n");
             } else if (mark>70){
-                System.print.out("B Grade\n");
+                System.out.print("B Grade\n");
             } else if (mark>60){
-                System.print.out("C Grade\n");
+                System.out.print("C Grade\n");
             } else if (mark>50){
-                System.print.out("D Grade\n");
+                System.out.print("D Grade\n");
             } else if (mark>40){
-                System.print.out("E Grade\n");
+                System.out.print("E Grade\n");
             } else if (mark>30){
-                System.print.out("P Grade\n");
+                System.out.print("P Grade\n");
             } else{
-                System.print.out("F Grade\n");
+                System.out.print("F Grade\n");
             }
         } else {
-            System.print.out("Wrong Input!!\n");
+            System.out.print("Wrong Input!!\n");
         }sc.close();
         
     }

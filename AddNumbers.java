fc2eaa4af069dae7c76a60d51b2c81235118cc18
Dario George <edu.dariogeorge21@gmail.com>
2025-07-17 08:19:05 +0530

    addNumbers

---
+++ AddNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+package add;
+
+
+public class AddNumbers{
+    
+    public static class main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.print.out("Enter number 1: ");
+        int num1 = scanner.nextInt();
+
+        System.print.out("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+        System.print.out("The sum of "+num1 " and "+num2+" is "+sum);
+        scanner.close();
+    }
+}

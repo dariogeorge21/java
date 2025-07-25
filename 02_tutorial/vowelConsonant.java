d454a91827a566db66464d0041e71e7eaa4ecdb8
Dario George <edu.dariogeorge21@gmail.com>
2025-07-25 21:05:05 +0530

    feat(tutorial): Vowel or Consonant

---
+++ 02_tutorial/vowelConsonant.java
@@ -0,0 +1,27 @@
+package tutorial;
+
+import java.util.Scanner;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+        switch(Character.toLowerCase(ch)) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " is a vowel.");
+                break;
+            default:
+                if (Character.isLetter(ch)) {
+                    System.out.println(ch + " is a consonant.");
+                } else {
+                    System.out.println(ch + " is not an alphabet character.");
+                }
+        }
+        sc.close();
+    }
+}
